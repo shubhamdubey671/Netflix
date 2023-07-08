a0c97115e9ce3138f523ff7ab66bc4c8d0b2b1ec
Shubham Dubey <shubhamdubey671@gmail.com>
2023-07-08 14:21:25 +0530

    testing new

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import stripe  from './stripe.js'
 import dotenv  from "dotenv"
 import cors from 'cors'
+const PORT = process.env.PORT || 5000
 
 dotenv.config()
 const app = express();
@@ -14,6 +15,6 @@ app.get("/",(req,res)=>{
     console.log("lets gooo!!!");
 })
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("server is running");
 })
